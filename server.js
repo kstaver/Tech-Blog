f0ca4836245600c92f63782cb2645f6e9b71aaaf
Kendra Staver <staverkendra@gmail.com>
2021-12-14 22:49:53 -0800

    basic variables for express and connections setup

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
