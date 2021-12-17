7653ffb36b70e74b0a24e51782e96af466e7f627
Kendra Staver <staverkendra@gmail.com>
2021-12-16 20:03:51 -0800

    outlined 8 blank comment seeds

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -1,8 +1,47 @@
 const { Comment } = require('../models');
 
-const commentData = {
-
-}
+const commentData = [
+    {
+        user_id: ,
+        post_id: ,
+        comment_text: ''
+    },
+    {
+        user_id: ,
+        post_id: ,
+        comment_text: ''
+    },
+    {
+        user_id: ,
+        post_id: ,
+        comment_text: ''
+    },
+    {
+        user_id: ,
+        post_id: ,
+        comment_text: ''
+    },
+    {
+        user_id: ,
+        post_id: ,
+        comment_text: ''
+    },
+    {
+        user_id: ,
+        post_id: ,
+        comment_text: ''
+    },
+    {
+        user_id: ,
+        post_id: ,
+        comment_text: ''
+    },
+    {
+        user_id: ,
+        post_id: ,
+        comment_text: ''
+    }
+];
 
 const seedComments = () => Comment.bulkCreate(commentData);
 
