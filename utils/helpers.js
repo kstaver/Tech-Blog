4dbd7a4d2028e8e19189bed4c4bc7ade6e5e9940
Kendra Staver <staverkendra@gmail.com>
2021-12-18 00:03:19 -0800

    added helpers

---
+++ utils/helpers.js
@@ -0,0 +1,13 @@
+module.exports = {
+    format_date: date => {
+        return `${new Data(data).getMonth() + 1}/${ new Data(data).getDate()}/${ new Data(
+            date
+        ).getFullYear()}`;
+        },
+    format_plural: (word, amount) => {
+        if (amount !== 1) {
+            return `${word}s`;
+        }
+        return word;
+    }
+}
