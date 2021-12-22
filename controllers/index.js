6ea0f6dc4e146a07e33fc3dac58c9087cf95cfec
Kendra Staver <staverkendra@gmail.com>
2021-12-21 19:34:28 -0800

    added connector to home and dashboard

--- controllers/index.js
+++ controllers/index.js
@@ -1,2 +1,15 @@
 const router = require('express').Router();
+
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
+const dashboardRoutes = require('./dashboard');
+
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
+
+router.use((req, res) => {
+    res.status(404).render('404');
+});
+
+module.exports = router;
