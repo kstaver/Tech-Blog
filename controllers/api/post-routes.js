c620e81d11829f13c01629efbb411ac3991bfc09
Kendra Staver <staverkendra@gmail.com>
2021-12-21 19:29:02 -0800

    finished home-routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -24,7 +24,7 @@ router.get('/', (req, res) => {
             {
                 model: User,
                 attributes: ['username', 'twitter', 'github']
-            },
+            }
         ]
     })
         .then(dbPostData => res.json(dbPostData))
