325ca44649eaa4987ad8b50085332400013d52d8
kstaver <staverkendra@gmail.com>
2022-01-08 11:05:18 -0800

    renamed handlebars to veiws

--- server.js
+++ server.js
@@ -27,13 +27,12 @@ const sess = {
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
 
 app.use(routes);
 
