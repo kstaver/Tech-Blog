de67bf24a887cf0b6e4ec6087c9cd776eec8648f
kstaver <staverkendra@gmail.com>
2022-01-16 21:43:04 -0800

    working on bugs

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.set('view engine', 'handlebars');
 app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
